5f1f944ece1c2335f756f823cea6de54fb047cb5
Dimitris Apostolou <dimitris.ap@live.com>
2018-11-14 19:27:13 +0200

    Update logout.js

--- src/main/resources/public/logout.js
+++ src/main/resources/public/logout.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#logoutButton').on('click', function(e){
         if(confirm("Are you sure you want to logout?"))
             sessionStorage.clear;
-            window.location.replace= "http://localhost:8080/index.html";
+            window.location.replace(ROOT_PATH + "/logout")
         return false;
     });
 });
