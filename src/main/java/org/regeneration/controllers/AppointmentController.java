d2c2a95be0664e529c8cf038a8961d34a06c264e
konskaff <kons.kaffe@gmail.com>
2018-11-21 13:31:04 +0200

    Citizen's frontend + custom dto

--- src/main/java/org/regeneration/controllers/AppointmentController.java
+++ src/main/java/org/regeneration/controllers/AppointmentController.java
@@ -1,4 +1,24 @@
 package org.regeneration.controllers;
 
+import org.regeneration.dto.DoctorSearchAppointmentsDTO;
+import org.regeneration.models.Appointment;
+import org.regeneration.repositories.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+
+@RestController
 public class AppointmentController {
+
 }
+
--- src/main/java/org/regeneration/dto/DoctorSearchAppointmentsDTO.java
+++ src/main/java/org/regeneration/dto/DoctorSearchAppointmentsDTO.java
@@ -7,4 +7,29 @@ public class DoctorSearchAppointmentsDTO {
     private String search;
 
 
+    public String getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(String fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public String getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(String toDate) {
+        this.toDate = toDate;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+
+
 }
