1d07600385bb774d531df59b730b0535df9aed33
CharaSar <sarianidou.chara@gmail.com>
2018-11-23 16:53:30 +0200

    LoggedInUserController and sessionStorage autosave role!

--- src/main/java/org/regeneration/controllers/LoggedInUserController.java
+++ src/main/java/org/regeneration/controllers/LoggedInUserController.java
@@ -1,4 +1,26 @@
 package org.regeneration.controllers;
 
+import org.regeneration.models.User;
+import org.regeneration.repositories.UserRepository;
+import org.regeneration.security.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
 public class LoggedInUserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/api/user")
+    public Role userRole(Principal principal){
+        User loggedInUser = userRepository.findByUsername(principal.getName());
+        return loggedInUser.getRole();
+    }
+
+
+
 }
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -7,21 +7,36 @@ function login(usernameElement,passwordElement) {
 	fd.append( 'password', password);
 
 	$.ajax({
-	url: ROOT_PATH + '/login',
-	data: fd,
-	processData: false,
-	contentType: false,
-	type: 'POST',
-	success: function(data){
-		sessionStorage.setItem(SESSION_STORAGE_LOGIN_TOKEN_NAME, username);
-		sessionStorage.setItem(SESSION_STORAGE_ROLE_NAME, data);//save user's role
-		window.location.replace(ROOT_PATH + "/users/doctor/index.html");
-	},
-	statusCode: {
-		401 : function() {
-			alert("Invalid username or password!");
-			}
-		}
+        url: ROOT_PATH + '/login',
+        data: fd,
+        processData: false,
+        contentType: false,
+        type: 'POST',
+        success: function(data){
+            $.ajax({
+                url:ROOT_PATH+'/api/user',
+                processData:false,
+                contentType:false,
+                type:'GET',
+                success:function(userdata, textStatus, jQxhr){
+                    if(userdata=="CITIZEN"){
+                        sessionStorage.setItem(SESSION_STORAGE_LOGIN_TOKEN_NAME, username);
+                        sessionStorage.setItem(SESSION_STORAGE_ROLE_NAME, userdata);//save user's role
+                        window.location.replace(ROOT_PATH + "/users/citizen/index.html");
+                    }else{
+                        sessionStorage.setItem(SESSION_STORAGE_LOGIN_TOKEN_NAME, username);
+                        sessionStorage.setItem(SESSION_STORAGE_ROLE_NAME, userdata);//save user's role
+                        window.location.replace(ROOT_PATH + "/users/doctor/index.html");
+                    }
+
+                }
+            });
+        },
+        statusCode: {
+            401 : function() {
+                alert("Invalid username or password!");
+                }
+            }
 	});
 
 }
